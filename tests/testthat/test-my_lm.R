b234fa6737ec628763c9c6654c0585003240508d
Chiang1 <m13013062879@163.com>
2022-06-09 01:32:19 +0800

    Set up package

---
+++ tests/testthat/test-my_lm.R
@@ -0,0 +1,18 @@
+# within test-my_lm.R
+data0 <- my_penguins %>%
+  dplyr::select(body_mass_g, bill_length_mm) %>%
+  tidyr::drop_na()
+data1 <- my_penguins %>%
+  dplyr::select( bill_length_mm) %>%
+  tidyr::drop_na()
+
+test_that("my_lm works mathematically", {
+  expect_type(my_lm(body_mass_g ~ bill_length_mm, data = data0), "double")
+  expect_is(my_lm(body_mass_g ~ bill_length_mm, data = data0), "matrix")
+})
+test_that("variables not included in data throws error", {
+  expect_error(my_lm(body_mass_g ~ bill_length_mm, data = data1))
+})
+test_that("data must be dataframe", {
+  expect_error(my_lm(body_mass_g ~ bill_length_mm, data = as.matrix(data0)))
+})
